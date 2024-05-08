fd5838382d8e640fcdad1e50a4f2c763cd724e45
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-05-08 10:22:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,10 +2,10 @@ from pyrogram import filters
 import os
 
 class Config:
-    API_ID = "24970012"
-    API_HASH = "43804289ce8eb7e37a1d680199cd2607"
+    API_ID = "27798659"
+    API_HASH = "26100c77cee02e5e34b2bbee58440f86"
     #TOKEN = "6521122303:AAGCO3XMjcA0SN5NAi1M0NpmbmMxEtwwYbg"
     TOKEN = os.environ.get("TOKEN", None)
-    MONGO_URL = "mongodb+srv://JARVISV1:JARVIS85@annie.bfgvyxi.mongodb.net/?retryWrites=true&w=majority&appName=Annie"
+    MONGO_URL = "mongodb+srv://jarvisdb:started@kishu.x6a4sr7.mongodb.net/?retryWrites=true&w=majority&appName=Kishu"
     START_PIC = "https://telegra.ph/file/0eba143d65f9413f9ae04.jpg"
-    SUDOERS = filters.user(["7157587567"])
+    SUDOERS = filters.user(["7044783841"])
